87b0514148c737ca397ff577eaf01d152eb8e57d
Luiz Gustavo <72631018+luizgustavoabreu@users.noreply.github.com>
2022-06-13 16:13:16 -0300

    Configurando os produtos como "Custom Post Type"
    e criação de produtos-orcamento.php que é usado como include em page-produtos.php e em single-produtos.php

---
+++ inc/produtos-orcamento.php
@@ -0,0 +1,16 @@
+<?php $contato = get_page_by_title('contato'); ?>
+
+<div class="orcamento_dados grid-8">
+  <h3>Dados</h3>
+  <span><?php the_field('telefone', $contato); ?></span>
+  <span><?php the_field('email', $contato); ?></span>
+  <h3>Monte a sua Bikcraft</h3>
+  <p>Escolha as especificações:</p>
+  <ul>
+    <li>- Cores</li>
+    <li>- Estilo</li>
+    <li>- Medidas</li>
+    <li>- Acessórios</li>
+    <li>- E Outros</li>
+  </ul>
+</div>
